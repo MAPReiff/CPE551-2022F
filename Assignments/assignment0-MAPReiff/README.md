135a7fa36f6d69b8594ae7b19b57b4714d80371e
Mitchell <me@mreiff.dev>
2022-09-04 22:57:31 -0400

    Assignment 0

---
+++ Assignments/assignment0-MAPReiff/README.md
@@ -0,0 +1,1 @@
+# Due Date: 10th September, 2022
---
+++ Assignments/assignment0-MAPReiff/answer.py
@@ -0,0 +1,17 @@
+def hello_world():
+    # Write some thing in the following doc string
+    """This text will go in the doc string for the hello world function"""
+
+    # Comment the following `line`
+    # Comment this line
+
+    # Print "Hello, World"
+    print("Hello World")
+
+
+def add(a, b):
+
+    # Write simple code to add variable a and b
+    output = a+b
+
+    return output
